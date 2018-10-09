17cb5d7e1a892013d380013f7160af4f99c4a42c
nima <ijinima@gmail.com>
2018-10-09 15:08:40 +0330

    AddBookActivity

--- app/src/main/java/ir/debook/debook/AddBookActivity.java
+++ app/src/main/java/ir/debook/debook/AddBookActivity.java
@@ -4,12 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import components.CustomButton;
+import components.CustomEditText;
+import components.CustomTextView;
+import components.SpecialEditText;
+
 public class AddBookActivity extends Activity {
+    SpecialEditText bookName,
+            author,
+            translator,
+            publisher,
+            editor,
+            yearOfPrint,
+            edition;
+    CustomButton addCategory;
+    CustomEditText price,originalPrice;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
-        // hello
+
 
     }
 }
